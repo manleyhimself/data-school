ac42529106fed1a30c529f9b78e188d5c40cfc70
manleyhimself <bcmanley1@gmail.com>
2013-12-29 01:23:31 -0500

    added env secret token'

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DataSchool::Application.config.secret_key_base = ENV["SOUP"] 
+DataSchool::Application.config.secret_key_base = ENV["SECRET_TOKEN"] 
