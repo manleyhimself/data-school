4ddcf5a0537b5bb5980a6eb91c39e058c47b7071
casunlight <vivian.stanford@gmail.com>
2014-03-02 23:53:03 -0500

    I add 3 new instructors and change the date of class.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DataSchool::Application.config.secret_key_base = ENV["SECRET_TOKEN"] 
+
+# DataSchool::Application.config.secret_key_base = ENV["SECRET_TOKEN"] 
+
+DataSchool::Application.config.secret_token = 'your current token'
+DataSchool::Application.config.secret_key_base = '3249db249709f7ae16d86ad4724e2216deb1a5bf6c4f9ae231a8c6d99400713fdd35a034b551bedad07ba019e7a73193b752c205e7a5fee09185c25bef2c1782' # this needs to be added
